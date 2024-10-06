9279ff28de3a9e89adbef271b8ee823d23d500a3
sonqs <49322724+sonqs@users.noreply.github.com>
2024-10-06 12:25:33 +0200

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
